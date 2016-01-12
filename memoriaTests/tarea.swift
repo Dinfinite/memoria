c16556e9878bdb260b55b60e98c5e074466caccc
Diego Perez <superdiegoshowdown@gmail.com>
2016-01-12 12:38:10 -0600

    hola

---
+++ memoriaTests/tarea.swift
@@ -0,0 +1,29 @@
+//
+//  memoriaTests.swift
+//  memoriaTests
+//
+//  Created by Diego Pérez on 1/11/16.
+//  Copyright © 2016 Diego Pérez. All rights reserved.
+
+
+var x = 0...100
+
+for r in x {
+    
+    if  r % 5 == 0 {
+        print ("\(r)  Bingo!!!")
+    }
+    else if r >= 30 && r <= 40 {
+        print ("\(r)  VivaSwift!!!")
+    }
+        
+    else if r % 2 == 0 {
+        print ("\(r)  par!!!")
+    }
+    else if r % 2 != 0 {
+        print ("\(r)  impar!!!")
+        
+        
+        
+    }
+}
